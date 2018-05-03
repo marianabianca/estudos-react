c61f234ac6711d4d86a3f8faab01b588830263a9
Mariana Dela-Bianca <mariana.bianca@ccc.ufcg.edu.br>
2018-05-03 01:26:49 -0300

    mais alteracoes projeto 3

--- terceiro-projeto/src/App.js
+++ terceiro-projeto/src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
 
-class Teste extends Component {
+class Botao extends Component {
   render() {
     return (
       <div>
-        <button onClick={() => this.props.onClick()}>Clica clica</button>
+        <button type="button" className="btn btn-outline-primary branco"
+         onClick={() => this.props.onClick()}>
+         { this.props.mensagem }
+         </button>
       </div>
     )
   }
@@ -22,25 +25,31 @@ class App extends Component {
     }
   }
 
-  handleClick () {
-    this.setState({
-      bla: !this.state.bla,
-    });
+  getCall () {
     axios.get('http://localhost:5000/')
-      .then(response => this.setState(console.log(response.data)))
-      // OLHAR ISSO https://stackoverflow.com/questions/33037116/how-to-map-an-objects-keys-to-make-json-easier-to-handle-in-react
+      .then(response => this.setState({bc: response.data}))
+  }
+
+  putCall () {
+    axios.put('http://localhost:5000/blockchain/closeblock')
+      .then(
+        response => this.setState({bc: response.data})
+      )  // TODO AJEITAR ISSO
   }
 
   render() {
     const nome = this.state.nome;
     const cond = this.state.bla;
-    const bcc = this.state.bc;
+    const items = this.state.bc;
     
     return (
       <div className="App">
-        <Teste onClick={() => this.handleClick()}/>
+        <Botao onClick={() => this.getCall()} mensagem="clica"/>
+        <Botao onClick={() => this.putCall()} mensagem="clica put"/>
           {cond && <div>{nome}</div>}
-          {bcc}
+        <ul>
+          {items.map(item => <li>{item.index}</li>)} {/* VER COMO FUNCIONA ESSES INDICES */}
+        </ul>
       </div>
     );
   }
